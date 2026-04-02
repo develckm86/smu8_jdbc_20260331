47622f555cb7483869d299fae85d8a8945c8ce8c
develckm86 <develckm86@gmail.com>
2026-04-02 13:02:29 +0900

    사원 조회 과제 생성

---
+++ src/com/sum8/ex/E06SelectEmpListDto.java
@@ -0,0 +1,5 @@
+package com.sum8.ex;
+
+public class E06SelectEmpListDto {
+    //사원 전체 조회를 결과를 List<L05EmpDto> 로 파싱하세요.
+}
---
+++ src/com/sum8/ex/E07DetailEmpDto.java
@@ -0,0 +1,5 @@
+package com.sum8.ex;
+
+public class E07DetailEmpDto {
+    //사원 번호로 단건조회 후 결과를 L05EmpDto 로 파싱하세요.
+}
